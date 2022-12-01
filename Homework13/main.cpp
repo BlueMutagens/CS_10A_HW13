84f5a2f1d2bf996b292471738a09e8b01602c9cf
Sean <sdbel@comcast.net>
2022-12-01 10:55:38 -0800

    Initial Commit

---
+++ Homework13/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework13
+//
+//  Created by Sean Belingheri on 12/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
